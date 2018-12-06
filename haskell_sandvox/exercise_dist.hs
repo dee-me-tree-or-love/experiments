e516ee7dc2d871a1312d0e55e2d69cbf8f55104b
dmitrii <dmitry-orlov@hotmail.com>
2018-12-06 21:13:50 +0100

    got doctest to work

--- haskell_sandvox/exercise_dist.hs
+++ haskell_sandvox/exercise_dist.hs
@@ -1,19 +1,14 @@
+module ExerciseDist where
+
 import Data.Time
 
-{-
-    Distribute exercises from the given set more or less evenly
--}
-
-
-{-
-    Available slots
--}
+-- | Available slots
+--
 nr_slots :: Int
 nr_slots = 7
 
-{- 
-    Given exercises 
--}
+-- | Given exercises 
+--
 exercise_bundle :: [(Int, String)]
 exercise_bundle = [
         (21, "Skills"), 
@@ -26,32 +21,40 @@ exercise_bundle = [
         (9, "Lambda")
     ]
 
-{-
-    Find total number of exercises
--}
+
+-- | Find total number of exercises
+--
 total_count :: [(Int, String)] -> Int
 total_count = foldr ((+) . fst) 0
 
-{-
-    Minimal required slot size
--}
+-- | Minimal required slot size
+--
 min_slot_size :: Int
 min_slot_size = ceiling (t / n) 
                     where   
                         t = (fromIntegral . total_count) exercise_bundle
                         n = fromIntegral nr_slots
 
-
-{-
-    Example: 
-    
-    >>>  distribute_tasks (2, "C") [[],[]] []
-    [[(1, "C")], [(1,"C")]]
--}
-
+-- | Places a task of a type once in each available bucket
+--
+-- Examples:
+--
+-- >>> distribute_tasks (2, "C") [[],[]] []
+-- [[(1,"C")],[(1,"C")]]
+--
+-- >>> distribute_tasks (3, "C") [[],[]] []
+-- [[(1,"C")],[(1,"C")]]
+--
+-- >>> distribute_tasks (1, "C") [[],[]] []
+-- [[(1,"C")],[]]
+--
+-- >>> distribute_tasks (1, "C") [[],[]] []
+-- [[(1,"C")],[]]
+--
+-- >>> distribute_tasks (3, "C") [[(1, "K")],[(1, "G")]] []
+-- [[(1,"K"),(1,"C")],[(1,"G"),(1,"C")]]
+--
 distribute_tasks :: (Int, String) -> [[(Int, String)]] -> [[(Int, String)]] -> [[(Int, String)]]
-distribute_tasks (0, _) bs acs = bs ++ acs
+distribute_tasks (0, _) bs acs = acs ++ bs 
 distribute_tasks _ [] acs = acs
 distribute_tasks (a, s) (b:bs) acs = distribute_tasks (a-1, s) bs (acs ++ [b ++ [(1, s)]])
-
-main = do putStrLn "whatever"
